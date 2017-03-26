dbf7d2816fb7d5a98038c2884a7a54d422233934
the-magister <magister@the-magister.com>
2017-03-25 17:40:33 -0700

    added palettes

--- Tree_Lights_3/Tree_Lights_3.ino
+++ Tree_Lights_3/Tree_Lights_3.ino
@@ -54,6 +54,91 @@ Settings s;
 CRGB newColor = CRGB::Black;
 CRGB currentColor = CRGB::Black;
 
+CRGBPalette16 currentPalette;
+
+// A mostly red palette with green accents and white trim.
+// "CRGB::Gray" is used as white to keep the brightness more uniform.
+const TProgmemRGBPalette16 RedGreenWhite_p FL_PROGMEM =
+{ CRGB::Gray, CRGB::Gray, CRGB::Red, CRGB::Red,
+  CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red,
+  CRGB::Red, CRGB::Red, CRGB::Gray, CRGB::Gray,
+  CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green
+};
+
+// A mostly (dark) green palette with red berries.
+#define Holly_Green 0x00580c
+#define Holly_Red   0xB00402
+const TProgmemRGBPalette16 Holly_p FL_PROGMEM =
+{ Holly_Green, Holly_Green, Holly_Green, Holly_Green,
+  Holly_Green, Holly_Green, Holly_Green, Holly_Green,
+  Holly_Green, Holly_Green, Holly_Green, Holly_Green,
+  Holly_Green, Holly_Green, Holly_Green, Holly_Red
+};
+
+// A red and white striped palette
+// "CRGB::Gray" is used as white to keep the brightness more uniform.
+const TProgmemRGBPalette16 RedWhite_p FL_PROGMEM =
+{ CRGB::Red,  CRGB::Red,  CRGB::Red,  CRGB::Red,
+  CRGB::Gray, CRGB::Gray, CRGB::Gray, CRGB::Gray,
+  CRGB::Red,  CRGB::Red,  CRGB::Red,  CRGB::Red,
+  CRGB::Gray, CRGB::Gray, CRGB::Gray, CRGB::Gray
+};
+
+// A mostly blue palette with white accents.
+// "CRGB::Gray" is used as white to keep the brightness more uniform.
+const TProgmemRGBPalette16 BlueWhite_p FL_PROGMEM =
+{ CRGB::Blue, CRGB::Blue, CRGB::Blue, CRGB::Blue,
+  CRGB::Blue, CRGB::Blue, CRGB::Blue, CRGB::Blue,
+  CRGB::Blue, CRGB::Blue, CRGB::Blue, CRGB::Blue,
+  CRGB::Blue, CRGB::Gray, CRGB::Gray, CRGB::Gray
+};
+
+// A pure "fairy light" palette with some brightness variations
+#define HALFFAIRY ((CRGB::FairyLight & 0xFEFEFE) / 2)
+#define QUARTERFAIRY ((CRGB::FairyLight & 0xFCFCFC) / 4)
+const TProgmemRGBPalette16 FairyLight_p FL_PROGMEM =
+{ CRGB::FairyLight, CRGB::FairyLight, CRGB::FairyLight, CRGB::FairyLight,
+  HALFFAIRY,        HALFFAIRY,        CRGB::FairyLight, CRGB::FairyLight,
+  QUARTERFAIRY,     QUARTERFAIRY,     CRGB::FairyLight, CRGB::FairyLight,
+  CRGB::FairyLight, CRGB::FairyLight, CRGB::FairyLight, CRGB::FairyLight
+};
+
+// A palette of soft snowflakes with the occasional bright one
+const TProgmemRGBPalette16 Snow_p FL_PROGMEM =
+{ 0x304048, 0x304048, 0x304048, 0x304048,
+  0x304048, 0x304048, 0x304048, 0x304048,
+  0x304048, 0x304048, 0x304048, 0x304048,
+  0x304048, 0x304048, 0x304048, 0xE0F0FF
+};
+
+// A palette reminiscent of large 'old-school' C9-size tree lights
+// in the five classic colors: red, orange, green, blue, and white.
+#define C9_Red    0xB80400
+#define C9_Orange 0x902C02
+#define C9_Green  0x046002
+#define C9_Blue   0x070758
+#define C9_White  0x606820
+const TProgmemRGBPalette16 RetroC9_p FL_PROGMEM =
+{ C9_Red,    C9_Orange, C9_Red,    C9_Orange,
+  C9_Orange, C9_Red,    C9_Orange, C9_Red,
+  C9_Green,  C9_Green,  C9_Green,  C9_Green,
+  C9_Blue,   C9_Blue,   C9_Blue,
+  C9_White
+};
+
+// A cold, icy pale blue palette
+#define Ice_Blue1 0x0C1040
+#define Ice_Blue2 0x182080
+#define Ice_Blue3 0x5080C0
+const TProgmemRGBPalette16 Ice_p FL_PROGMEM =
+{
+  Ice_Blue1, Ice_Blue1, Ice_Blue1, Ice_Blue1,
+  Ice_Blue1, Ice_Blue1, Ice_Blue1, Ice_Blue1,
+  Ice_Blue1, Ice_Blue1, Ice_Blue1, Ice_Blue1,
+  Ice_Blue2, Ice_Blue2, Ice_Blue2, Ice_Blue3
+};
+
+
 CRGB getCheerLightsColor() {
   static CRGB lastColor = CRGB::Black;
 
@@ -204,6 +289,17 @@ void returnForm() {
   message += radioInput("Color", "26", s.color == 26, "Goldenrod");
   message += "<br><br>";
 
+  message += "Palettes: ";
+  message += radioInput("Color", "50", s.color == 50, "RedGreenWhite");
+  message += radioInput("Color", "51", s.color == 51, "Holly");
+  message += radioInput("Color", "52", s.color == 52, "RedWhite");
+  message += radioInput("Color", "53", s.color == 53, "BlueWhite");
+  message += radioInput("Color", "54", s.color == 54, "FairyLight");
+  message += radioInput("Color", "55", s.color == 55, "Snow");
+  message += radioInput("Color", "56", s.color == 56, "RetroC9");
+  message += radioInput("Color", "57", s.color == 57, "Ice");
+  message += "<br><br>";
+
   message += "<INPUT type=\"submit\" value=\"Update Lights\">";
   message += "</P>";
   message += "</FORM>";
@@ -289,6 +385,15 @@ void updateFromSettings() {
     case 25: newColor = CRGB::LightSlateGray; break;
     case 26: newColor = CRGB::Goldenrod; break;
 
+    // Palettes
+    case 50: currentPalette = RedGreenWhite_p; break;
+    case 51: currentPalette = Holly_p; break;
+    case 52: currentPalette = RedWhite_p; break;
+    case 53: currentPalette = BlueWhite_p; break;
+    case 54: currentPalette = FairyLight_p; break;
+    case 55: currentPalette = Snow_p; break;
+    case 56: currentPalette = RetroC9_p; break;
+    case 57: currentPalette = Ice_p; break;
   }
 
   // set master brightness control
@@ -310,39 +415,52 @@ void addSparkles(fract8 chanceOfGlitter) {
 
 void animations() {
   static byte maxBlend = 1;
-
+  static byte colorIndex = 0;
+  
   // do some periodic updates
   EVERY_N_MILLISECONDS( 1000UL / FRAMES_PER_SECOND ) {
+    blueOn();
 
-    // perform a blend across all leds to the new color
-    if ( currentColor != newColor ) {
-      //      Serial << currentColor.red << F("/") << currentColor.green << F("/") << currentColor.blue;
-      //      Serial << F(" -> ");
-      //      Serial << newColor.red << F("/") << newColor.green << F("/") << newColor.blue;
-      //      Serial << endl;
+    if( s.color >= 50 ) {
+      // pallettes
+      colorIndex ++;
+      byte j = 0;
+      for( int i = 0; i < n_led; i++) {
+        leds[i] = ColorFromPalette( currentPalette, colorIndex+j, 255, LINEARBLEND);        
+        j++;
+      }
+    } else { 
+      // solids
 
-      blueOn();
-
-      // do a shift, but not for too long
-      maxBlend = qadd8(maxBlend, 1);
-      if ( maxBlend < 255 ) currentColor = blend(currentColor, newColor, maxBlend);
-      else currentColor = newColor;
-
-    } else {
-      maxBlend = 1;
+      // perform a blend across all leds to the new color
+      if ( currentColor != newColor ) {
+        //      Serial << currentColor.red << F("/") << currentColor.green << F("/") << currentColor.blue;
+        //      Serial << F(" -> ");
+        //      Serial << newColor.red << F("/") << newColor.green << F("/") << newColor.blue;
+        //      Serial << endl;  
+ 
+        // do a shift, but not for too long
+        maxBlend = qadd8(maxBlend, 1);
+        if ( maxBlend < 255 ) currentColor = blend(currentColor, newColor, maxBlend);
+        else currentColor = newColor;
+  
+      } else {
+        maxBlend = 1;
+      }
+  
+      // render
+      fill_solid(leds, n_led, currentColor);
     }
-
-    // render
-    fill_solid(leds, n_led, currentColor);
-
+    
     // add some glitter
     if ( s.color != 0 && s.sparkles > 0 ) addSparkles(s.sparkles);
 
-    blueOff();
   }
 
   // send the 'leds' array out to the actual LED strip
   FastLED.show();
+
+  blueOff();
 }
 
 void redOff() {
@@ -614,88 +732,6 @@ void coolLikeIncandescent( CRGB& c, uint8_t phase)
   c.g = qsub8( c.g, cooling);
   c.b = qsub8( c.b, cooling * 2);
 }
-
-// A mostly red palette with green accents and white trim.
-// "CRGB::Gray" is used as white to keep the brightness more uniform.
-const TProgmemRGBPalette16 RedGreenWhite_p FL_PROGMEM =
-{ CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red,
-  CRGB::Red, CRGB::Red, CRGB::Red, CRGB::Red,
-  CRGB::Red, CRGB::Red, CRGB::Gray, CRGB::Gray,
-  CRGB::Green, CRGB::Green, CRGB::Green, CRGB::Green
-};
-
-// A mostly (dark) green palette with red berries.
-#define Holly_Green 0x00580c
-#define Holly_Red   0xB00402
-const TProgmemRGBPalette16 Holly_p FL_PROGMEM =
-{ Holly_Green, Holly_Green, Holly_Green, Holly_Green,
-  Holly_Green, Holly_Green, Holly_Green, Holly_Green,
-  Holly_Green, Holly_Green, Holly_Green, Holly_Green,
-  Holly_Green, Holly_Green, Holly_Green, Holly_Red
-};
-
-// A red and white striped palette
-// "CRGB::Gray" is used as white to keep the brightness more uniform.
-const TProgmemRGBPalette16 RedWhite_p FL_PROGMEM =
-{ CRGB::Red,  CRGB::Red,  CRGB::Red,  CRGB::Red,
-  CRGB::Gray, CRGB::Gray, CRGB::Gray, CRGB::Gray,
-  CRGB::Red,  CRGB::Red,  CRGB::Red,  CRGB::Red,
-  CRGB::Gray, CRGB::Gray, CRGB::Gray, CRGB::Gray
-};
-
-// A mostly blue palette with white accents.
-// "CRGB::Gray" is used as white to keep the brightness more uniform.
-const TProgmemRGBPalette16 BlueWhite_p FL_PROGMEM =
-{ CRGB::Blue, CRGB::Blue, CRGB::Blue, CRGB::Blue,
-  CRGB::Blue, CRGB::Blue, CRGB::Blue, CRGB::Blue,
-  CRGB::Blue, CRGB::Blue, CRGB::Blue, CRGB::Blue,
-  CRGB::Blue, CRGB::Gray, CRGB::Gray, CRGB::Gray
-};
-
-// A pure "fairy light" palette with some brightness variations
-#define HALFFAIRY ((CRGB::FairyLight & 0xFEFEFE) / 2)
-#define QUARTERFAIRY ((CRGB::FairyLight & 0xFCFCFC) / 4)
-const TProgmemRGBPalette16 FairyLight_p FL_PROGMEM =
-{ CRGB::FairyLight, CRGB::FairyLight, CRGB::FairyLight, CRGB::FairyLight,
-  HALFFAIRY,        HALFFAIRY,        CRGB::FairyLight, CRGB::FairyLight,
-  QUARTERFAIRY,     QUARTERFAIRY,     CRGB::FairyLight, CRGB::FairyLight,
-  CRGB::FairyLight, CRGB::FairyLight, CRGB::FairyLight, CRGB::FairyLight
-};
-
-// A palette of soft snowflakes with the occasional bright one
-const TProgmemRGBPalette16 Snow_p FL_PROGMEM =
-{ 0x304048, 0x304048, 0x304048, 0x304048,
-  0x304048, 0x304048, 0x304048, 0x304048,
-  0x304048, 0x304048, 0x304048, 0x304048,
-  0x304048, 0x304048, 0x304048, 0xE0F0FF
-};
-
-// A palette reminiscent of large 'old-school' C9-size tree lights
-// in the five classic colors: red, orange, green, blue, and white.
-#define C9_Red    0xB80400
-#define C9_Orange 0x902C02
-#define C9_Green  0x046002
-#define C9_Blue   0x070758
-#define C9_White  0x606820
-const TProgmemRGBPalette16 RetroC9_p FL_PROGMEM =
-{ C9_Red,    C9_Orange, C9_Red,    C9_Orange,
-  C9_Orange, C9_Red,    C9_Orange, C9_Red,
-  C9_Green,  C9_Green,  C9_Green,  C9_Green,
-  C9_Blue,   C9_Blue,   C9_Blue,
-  C9_White
-};
-
-// A cold, icy pale blue palette
-#define Ice_Blue1 0x0C1040
-#define Ice_Blue2 0x182080
-#define Ice_Blue3 0x5080C0
-const TProgmemRGBPalette16 Ice_p FL_PROGMEM =
-{
-  Ice_Blue1, Ice_Blue1, Ice_Blue1, Ice_Blue1,
-  Ice_Blue1, Ice_Blue1, Ice_Blue1, Ice_Blue1,
-  Ice_Blue1, Ice_Blue1, Ice_Blue1, Ice_Blue1,
-  Ice_Blue2, Ice_Blue2, Ice_Blue2, Ice_Blue3
-};
 
 
 // Add or remove palette names from this list to control which color
